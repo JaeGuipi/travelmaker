8ce96ca3c02cfbaff3646fad8ac19c10ae385da1
jjeankim <miyukikj35@gmail.com>
2024-11-23 12:51:00 +0900

    Feat:Next.js @14 프로젝트 생성

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      HOME 페이지
+      <p className={styles.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur tenetur odio quis commodi perspiciatis, autem vel, eveniet consequuntur sint ea fuga, officia facilis illo dolorum expedita eaque iusto repellat blanditiis?</p>
+    </div>
+  );
+}
