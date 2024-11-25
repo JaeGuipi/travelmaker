59d0725e2fd47f158cd97e4a5d9b0ce773dd8480
99uminji <rnalswl96@naver.com>
2024-11-25 21:36:04 +0900

    Fix: API_URL 변수 생성 및 activities 폴더 생성

---
+++ src/lib/api/activities.ts
@@ -0,0 +1,36 @@
+import { GetMyActivities, PostActivites } from "@/app/types/types";
+import { API_URL } from "@/constants/config";
+
+// 체험 리스트 조회
+export const getActivities = async (): Promise<GetMyActivities> => {
+  try {
+    const response = await fetch(`${API_URL}/activities?method=offset`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+};
+
+// 체험 리스트 등록
+export const postActivities = async (activeData: PostActivites) => {
+  try {
+    const response = await fetch(`${API_URL}/activities`, {
+      method: "POST",
+      body: JSON.stringify(activeData),
+    });
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+};
+
+// 체험 상세 조회
+// 체험 예약 가능일 조회
+// 체험 리뷰 조회
+// 체험 예약 신청
+// 체험 이미지 url 생성
