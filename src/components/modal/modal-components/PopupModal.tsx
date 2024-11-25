9a53db6e660dae872b1004a4a75b17f0fff08294
99uminji <rnalswl96@naver.com>
2024-11-25 14:19:27 +0900

    Fix: FormButton onClick 추가

--- src/components/modal/modal-components/PopupModal.tsx
+++ src/components/modal/modal-components/PopupModal.tsx
@@ -8,7 +8,9 @@ export default function PopupModal({ text }) {
     <>
       <p>{text}</p>
       {/* 공통 버튼 컴포넌트 사용 예정*/}
-      <button onClick={() => toggleModal("PopupModal")}>확인</button>
+      <FormButton size="medium" onClick={() => toggleModal("PopupModal")}>
+        확인
+      </FormButton>
     </>
   );
 }
