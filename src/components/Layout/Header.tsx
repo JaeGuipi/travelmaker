b4b76dee2634bfa494a74c64aee791df27689747
JIHYEMIN <hyemeeeny@gmail.com>
2024-11-25 18:46:47 +0900

    Feat: Header 컴포넌트 추가

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import classNames from "classnames/bind";
+import s from "./Header.module.scss";
+import { useAuth } from "@/hooks/useAuth";
+
+export const cx = classNames.bind(s);
+
+const Header = () => {
+  const { user, logout } = useAuth();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen((prev) => !prev);
+  };
+
+  return (
+    <header className={cx("headerContainer", "container")}>
+      <Link href={"/"} className={s.logo}>
+        <Image src={"/images/logo.png"} fill alt="TRAVEL MAKER" />
+      </Link>
+
+      <div className={s.userWrap}>
+        {user ? (
+          <div className={s.loginOn}>
+            <button>
+              <Image src={"/icons/btn_alarm.svg"} width={15} height={17} alt="알림" />
+            </button>
+            <div className={s.userInfo} onClick={toggleDropdown}>
+              <Image className={s.profile} src={"/images/profile.png"} width={32} height={32} alt="프로필" />
+              <p className={s.name}>정만철</p>
+
+              {isDropdownOpen && (
+                <div className={s.logout}>
+                  <button onClick={() => logout()}>로그아웃</button>
+                </div>
+              )}
+            </div>
+          </div>
+        ) : (
+          <div className={s.loginOff}>
+            <Link href={"/login"}>로그인</Link>
+            <Link href={"/signup"}>회원가입</Link>
+          </div>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
