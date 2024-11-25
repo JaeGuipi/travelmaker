797186c5372cdebf29451db24b2fd93db3ff7b8e
99uminji <rnalswl96@naver.com>
2024-11-25 23:09:41 +0900

    Feat: activities api 함수 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import { getActivity } from "@/lib/api/activities";
 import s from "./page.module.scss";
 
 export default async function Home() {
-  const activities = await getActivity();
-  return (
-    <div className={s.page}>
-      <div>{JSON.stringify(activities, null, 2)}</div>HOME 페이지
-    </div>
-  );
+  return <div className={s.page}>HOME 페이지</div>;
 }
--- src/lib/api/activities.ts
+++ src/lib/api/activities.ts
@@ -1,8 +1,18 @@
-import { GetMyActivities, PostActivites } from "@/app/types/types";
+import {
+  GetActivities,
+  PostActivity,
+  PostActivityResponse,
+  ActivityDetail,
+  GetReviews,
+  PostReservation,
+  ScheduleList,
+  PostReservationResponse,
+} from "@/app/types/activites/activitesTypes";
+import { PostImage, PostImageResponse } from "@/app/types/types";
 import { API_URL } from "@/constants/config";
 
 // 체험 리스트 조회
-export const getActivity = async (): Promise<GetMyActivities> => {
+export const getActivity = async (): Promise<GetActivities> => {
   try {
     const response = await fetch(`${API_URL}/activities?method=offset`, {
       method: "GET",
@@ -17,7 +27,7 @@ export const getActivity = async (): Promise<GetMyActivities> => {
 };
 
 // 체험 리스트 등록
-export const postActivity = async (activeData: PostActivites) => {
+export const postActivity = async (activeData: PostActivity): Promise<PostActivityResponse> => {
   try {
     const response = await fetch(`${API_URL}/activities`, {
       method: "POST",
@@ -30,9 +40,9 @@ export const postActivity = async (activeData: PostActivites) => {
 };
 
 // 체험 상세 조회
-export const getActivityDetail = async (): Promise<GetMyActivities> => {
+export const getActivityDetail = async (activityId: number): Promise<ActivityDetail> => {
   try {
-    const response = await fetch(`${API_URL}/activities?method=offset`, {
+    const response = await fetch(`${API_URL}/activities/${activityId}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -43,7 +53,73 @@ export const getActivityDetail = async (): Promise<GetMyActivities> => {
     throw error;
   }
 };
+
 // 체험 예약 가능일 조회
+export const getAvailableSchedule = async (activityId: number, year: string, month: string): Promise<ScheduleList> => {
+  try {
+    const response = await fetch(`${API_URL}/activities/${activityId}/available-schedule?year=${year}&month=${month}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+};
+
 // 체험 리뷰 조회
+export const getActivityReview = async (activityId: number): Promise<GetReviews> => {
+  try {
+    const response = await fetch(`${API_URL}/activities/${activityId}/reviews`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+};
+
 // 체험 예약 신청
+export const postReservation = async (
+  activityId: number,
+  reservationData: PostReservation,
+): Promise<PostReservationResponse> => {
+  try {
+    const response = await fetch(`${API_URL}/activities/${activityId}/reservations`, {
+      method: "POST",
+      body: JSON.stringify(reservationData),
+    });
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+};
+
 // 체험 이미지 url 생성
+export const createActivityImage = async ({ file }: PostImage): Promise<PostImageResponse> => {
+  if (!file) throw new Error("이미지가 없습니다.");
+
+  const formData = new FormData();
+  formData.append("image", file);
+
+  try {
+    const response = await fetch(`${API_URL}/activities/image`, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || "이미지 업로드 실패");
+    }
+
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+};
