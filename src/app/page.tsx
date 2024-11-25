6bbcae9148f51439855b5584960607fdc4071169
99uminji <rnalswl96@naver.com>
2024-11-25 10:19:06 +0900

    Feat: 함수 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+import { FetchActivitiesParams, getActivities } from "./lib/api/Activities";
 import s from "./page.module.scss";
 
-export default function Home() {
+export default async function Home() {
+  const params: FetchActivitiesParams = {
+    method: "offset",
+    page: 1,
+    size: 20,
+  };
+
+  const activities = await getActivities(params);
+  console.log(activities);
   return (
     <div className={s.page}>
       HOME 페이지
