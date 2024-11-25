82ea7bffc352ceb9b3ed5c53c1a428649f8352e3
JaeGuipi <81738650+JaeGuipi@users.noreply.github.com>
2024-11-25 18:43:36 +0900

    Feat: API 내 예약 관련 함수 #27 수정

--- src/utils/customFetch.ts
+++ src/utils/customFetch.ts
@@ -1,21 +1,21 @@
-// utils/customFetch.ts
-export const customFetch = async (input: RequestInfo, init?: RequestInit) => {
-  const accessToken = localStorage.getItem("accessToken");
-  const refreshToken = localStorage.getItem("refreshToken");
+import { cookies } from "next/headers";
 
-  const defaultHeaders = {
-    "Content-Type": "application/json",
+export const customFetch = async (input: RequestInfo | URL, init?: RequestInit) => {
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+
+  const defaultHeaders: HeadersInit = {
     ...(accessToken && { Authorization: `Bearer ${accessToken}` }),
-    ...(refreshToken && { "x-refresh-token": refreshToken }),
   };
 
-  const customInit = {
+  const customInit: RequestInit = {
     ...init,
     headers: {
       ...defaultHeaders,
       ...(init?.headers || {}),
     },
+    cache: "no-store", // 최신 데이터 가져오기 Next.js 14버전은 자동 캐시가 디폴트여서 no-store로 설정. 이렇게 안하면 캐시로 인해 데이터가 안바뀌는 경우가 생김
   };
 
-  return fetch(input, customInit);
+  return await fetch(input, customInit);
 };
