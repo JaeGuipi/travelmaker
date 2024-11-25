f6605dc18b4249e4f763c57a25cda86c07602ec3
99uminji <rnalswl96@naver.com>
2024-11-25 21:36:04 +0900

    Fix: API_URL 변수 생성 및 activities 폴더 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
-import { FetchActivitiesParams, getActivities } from "./lib/api/Activities";
+import { getActivities } from "@/lib/api/activities";
 import s from "./page.module.scss";
 
 export default async function Home() {
-  const params: FetchActivitiesParams = {
-    method: "offset",
-    page: 1,
-    size: 20,
-  };
-
-  const activities = await getActivities(params);
-  console.log(activities);
+  const activities = await getActivities();
+  console.log("콘솔", activities);
   return (
     <div className={s.page}>
-      HOME 페이지
+      <div>{JSON.stringify(activities, null, 2)}</div>HOME 페이지
     </div>
   );
 }
