4abecd8dc3c2bb8598ec356c9fe3ed0ac3f66359
99uminji <rnalswl96@naver.com>
2024-11-26 11:55:26 +0900

    Fix: cx bind 사용 & 컴포넌트 폴더명 대문자로 변경

--- src/components/modal/modal-components/ConfirmModal.tsx
+++ src/components/modal/modal-components/ConfirmModal.tsx
@@ -3,6 +3,9 @@ import FormButton from "@/components/Button/FormButton";
 import s from "./ModalStyle.module.scss";
 import ModalContainer from "../ModalContainer";
 import Image from "next/image";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(s);
 
 interface ModalProps {
   text: string;
@@ -18,7 +21,7 @@ const ConfirmModal = ({ text }: ModalProps) => {
           <Image src="/icons/btn_check.svg" fill alt={text} />
         </span>
         <p className={s.text}>{text}</p>
-        <div className={`${s["button-wrap"]} ${s.flex}`}>
+        <div className={cx("button-wrap", "flex")}>
           <FormButton size="small" variant="emptyButton" onClick={() => toggleModal(text)}>
             아니오
           </FormButton>
