3786b1149795b0c90dffb422d937b471d867ee61
jjeankim <miyukikj35@gmail.com>
2024-11-26 17:02:25 +0900

    Feat: myActivities api 함수 제작

---
+++ src/app/types/myActivitiesTypes/myActivitiesTypes.ts
@@ -0,0 +1,40 @@
+//내 체험 수정
+export interface PatchMyActivities {
+  title: string;
+  category: string;
+  description: string;
+  price: number;
+  address: string;
+  bannerImageUrl: string;
+  subImageIdsToRemove: [];
+  subImageUrlsToAdd: [];
+  scheduleIdsToRemove: [];
+  schedulesToAdd: [];
+}
+
+//내 체험 예약 상태(승인,거절)업데이트
+export type PatchMyActivitiesByReservations = {
+  status: "pending" | "confirm" | "decline";
+};
+
+export interface getMyActivitiesByDashboardQuery {
+  activityId: string;
+  year: string;
+  month: string;
+}
+
+export interface getMyActivitiesByScheduleQuery {
+  activityId: string;
+  date: string;
+}
+
+export interface getMyActivitiesByReservationsQuery {
+  activityId: string;
+  scheduleId: string;
+  status: "declined" | "pending" | "confirmed";
+}
+
+export interface updateReservationStatusQuery {
+  activityId: string;
+  reservationId: string;
+}
