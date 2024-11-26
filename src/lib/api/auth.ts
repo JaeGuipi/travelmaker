aa36dd3880628627040bcc7435c97b1ae6cb075b
JIHYEMIN <hyemeeeny@gmail.com>
2024-11-26 17:11:55 +0900

    Refactor: auth api 수정

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -33,10 +33,6 @@ export const loginUser = async ({ email, password }: PostAuth) => {
 
     const data = await response.json();
 
-    console.log("데이터", data);
-    console.log("액세스", data.accessToken);
-    console.log("리프레시", data.refreshToken);
-
     setCookie("accessToken", data.accessToken);
     setCookie("refreshToken", data.refreshToken);
 
@@ -48,14 +44,12 @@ export const loginUser = async ({ email, password }: PostAuth) => {
 };
 
 export const authTokens = async () => {
-  const refreshToken = cookies().get("refreshToken");
+  const refreshToken = cookies().get("refreshToken")?.value;
 
   if (!refreshToken) {
-    console.log("리프레시 토큰이 없습니다.");
-    throw new Error("Refresh token is missing.");
+    console.error("refreshToken이 없습니다.");
+    throw new Error("인증되지 않은 사용자");
   }
-
-  console.log("현재 리프레시 토큰:", refreshToken);
 
   try {
     const response = await fetch(`${API_URL}/auth/tokens`, {
@@ -67,25 +61,16 @@ export const authTokens = async () => {
     });
 
     if (!response.ok) {
-      console.log("토큰 재발급 실패");
-      throw new Error("Failed to refresh tokens");
+      console.error("refreshToken 재발급 에러 발생");
+      throw new Error("refreshToken 재발급 실패");
     }
 
-    // 성공적으로 응답을 받은 경우, JSON으로 파싱
     const { accessToken } = await response.json();
-
-    console.log("새로운 액세스 토큰:", accessToken);
-    //console.log("새로운 리프레시 토큰:", newRefreshToken);
-
-    // 쿠키에 새로운 토큰들 저장
     setCookie("accessToken", accessToken);
-    //setCookie("refreshToken", newRefreshToken);
-
-    console.log("새로운 토큰 저장 완료");
 
     return { success: true, message: "refreshToken 요청 성공" };
   } catch (error) {
-    console.error("refreshToken 에러 발생:", error);
+    console.error("refreshToken 에러 발생: ", error);
     throw new Error("refreshToken 요청 실패");
   }
 };
