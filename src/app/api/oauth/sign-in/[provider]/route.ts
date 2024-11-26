5e437434dc58286e292786f25e5e5160771fbd38
JIHYEMIN <hyemeeeny@gmail.com>
2024-11-26 22:14:04 +0900

    Refactor: 간편 회원가입, 로그인 수정

--- src/app/api/oauth/sign-in/[provider]/route.ts
+++ src/app/api/oauth/sign-in/[provider]/route.ts
@@ -2,22 +2,38 @@ import { NextRequest, NextResponse } from "next/server";
 
 // Google 토큰 검증
 async function verifyGoogleToken(token: string) {
-  const response = await fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${token}`);
-  const data = await response.json();
-  return !data.error; // 유효한 토큰이면 true, 아니면 false
+  try {
+    const response = await fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${token}`);
+    if (!response.ok) {
+      return null;
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Google 토큰 검증 중 오류:", error);
+    return null;
+  }
 }
 
 // Kakao 토큰 검증
 async function verifyKakaoToken(token: string) {
-  const response = await fetch(`https://kapi.kakao.com/v2/user/me`, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  const data = await response.json();
-  return data.id ? true : false; // 유효한 사용자 정보가 있으면 true
+  try {
+    const response = await fetch(`https://kapi.kakao.com/v2/user/me`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (!response.ok) {
+      return null;
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Kakao 토큰 검증 중 오류:", error);
+    return null;
+  }
 }
+
+const homeRedirectUri = "/";
 
 // POST 요청 처리
 export async function POST(request: NextRequest, { params }: { params: { provider: string } }) {
@@ -29,30 +45,27 @@ export async function POST(request: NextRequest, { params }: { params: { provide
     return NextResponse.json({ message: "필수 항목이 누락되었습니다." }, { status: 400 });
   }
 
-  // 제공자별 토큰 검증
-  let isValidToken = false;
+  // 로그인 제공자별 토큰 검증: provider 값에 따라 각각의 검증 함수를 호출
+  let userData;
   if (provider === "google") {
-    isValidToken = await verifyGoogleToken(token);
+    userData = await verifyGoogleToken(token);
   } else if (provider === "kakao") {
-    isValidToken = await verifyKakaoToken(token);
+    userData = await verifyKakaoToken(token);
   } else {
     return NextResponse.json({ message: "지원하지 않는 로그인 제공자입니다." }, { status: 400 });
   }
 
-  // 토큰 검증 실패
-  if (!isValidToken) {
+  // 토큰 검증 실패 처리
+  if (userData.error || !userData.id) {
     return NextResponse.json({ message: "토큰이 유효하지 않습니다." }, { status: 401 });
   }
 
-  // 로그인 성공 후 리디렉션 URI 반환
+  // 로그인 성공 후 메인 화면으로 리디렉션
   return NextResponse.json(
     {
       message: "로그인 성공",
-      redirectUri:
-        redirectUri ||
-        (provider === "google"
-          ? process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI_SIGN_IN
-          : process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI_SIGN_IN),
+      redirectUri: redirectUri || homeRedirectUri,
+      user: userData,
     },
     { status: 200 },
   );
