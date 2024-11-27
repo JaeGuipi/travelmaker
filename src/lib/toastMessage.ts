8f7ce71ec60508662f43c2779335c74c4ec00bcf
JIHYEMIN <hyemeeeny@gmail.com>
2024-11-27 17:23:10 +0900

    Chore: 토스트 메시지 수정

--- src/lib/toastMessage.ts
+++ src/lib/toastMessage.ts
@@ -1,15 +1,15 @@
 const toastMessages = {
   success: {
-    login: "로그인에 성공했습니다",
-    signup: "회원가입에 성공했습니다",
+    login: "로그인에 성공하였습니다.",
+    signup: "회원가입에 성공하였습니다.",
     editPassword: "비밀번호가 변경되었습니다",
     updateProfile: "프로필이 업데이트 되었습니다",
   },
   error: {
     login: "로그인에 실패하였습니다",
-    signup: "가입에 실패하였습니다",
+    signup: "회원가입에 실패하였습니다",
     editPassword: "비밀번호 변경이 실패하였습니다",
-    updateProfile: "프로필 업데이트 실패하였습니다",
+    updateProfile: "프로필 업데이트에 실패하였습니다",
   },
 };
 
