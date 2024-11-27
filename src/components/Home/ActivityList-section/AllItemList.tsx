3e08c92a35945bb73e44d32d8589e648eef7bfb5
99uminji <rnalswl96@naver.com>
2024-11-27 23:26:02 +0900

    Design: 카테고리 css 수정

--- src/components/Home/ActivityList-section/AllItemList.tsx
+++ src/components/Home/ActivityList-section/AllItemList.tsx
@@ -2,29 +2,40 @@ import { GetActivities } from "@/types/activites/activitesTypes";
 import s from "./AllItemList.module.scss";
 import Link from "next/link";
 import Image from "next/image";
+import FormButton from "@/components/Button/FormButton";
 
 const AllItemList: React.FC<GetActivities> = ({ activities = [], totalCount }) => {
   console.log(totalCount);
   return (
-    <ul className={s["item-list"]}>
-      {activities.map((activity) => (
-        <li key={activity.id} className={s["item-box"]}>
-          <Link href="/">
-            <Image src={activity.bannerImageUrl || `/images/profile.png`} width={283} height={283} alt="체험 이미지" />
-            <div className={s.info}>
-              <p className={s.rating}>
-                <span>Rating icon</span>: {activity.rating} <strong>({activity.reviewCount})</strong>
-              </p>
-              <h4 className={s.title}>{activity.title}</h4>
-              <p className={s.price}>
-                ${activity.price}
-                <span> / 인</span>
-              </p>
-            </div>
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className={s["item-list"]}>
+        {activities.map((activity) => (
+          <li key={activity.id} className={s["item-box"]}>
+            <Link href="/">
+              <Image
+                src={activity.bannerImageUrl || `/images/profile.png`}
+                width={283}
+                height={283}
+                alt="체험 이미지"
+              />
+              <div className={s.info}>
+                <p className={s.rating}>
+                  <span>Rating icon</span>: {activity.rating} <strong>({activity.reviewCount})</strong>
+                </p>
+                <h4 className={s.title}>{activity.title}</h4>
+                <p className={s.price}>
+                  ${activity.price}
+                  <span> / 인</span>
+                </p>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <FormButton size="medium" variant="emptyButton">
+        더보기
+      </FormButton>
+    </>
   );
 };
 
