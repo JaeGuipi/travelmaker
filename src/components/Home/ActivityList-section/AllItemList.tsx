6d29a6a03b04de308a4f0a24c7d14143679308e1
99uminji <rnalswl96@naver.com>
2024-11-27 23:08:40 +0900

    Feat: 카테고리 리스트 ui 작업

--- src/components/Home/ActivityList-section/AllItemList.tsx
+++ src/components/Home/ActivityList-section/AllItemList.tsx
@@ -1,12 +1,28 @@
 import { GetActivities } from "@/types/activites/activitesTypes";
-import React from "react";
+import s from "./AllItemList.module.scss";
+import Link from "next/link";
+import Image from "next/image";
 
 const AllItemList: React.FC<GetActivities> = ({ activities = [], totalCount }) => {
   console.log(totalCount);
   return (
-    <ul>
+    <ul className={s["item-list"]}>
       {activities.map((activity) => (
-        <li key={activity.id}>{activity.title}</li>
+        <li key={activity.id} className={s["item-box"]}>
+          <Link href="/">
+            <Image src={activity.bannerImageUrl || `/images/profile.png`} width={283} height={283} alt="체험 이미지" />
+            <div className={s.info}>
+              <p className={s.rating}>
+                <span>Rating icon</span>: {activity.rating} <strong>({activity.reviewCount})</strong>
+              </p>
+              <h4 className={s.title}>{activity.title}</h4>
+              <p className={s.price}>
+                ${activity.price}
+                <span> / 인</span>
+              </p>
+            </div>
+          </Link>
+        </li>
       ))}
     </ul>
   );
