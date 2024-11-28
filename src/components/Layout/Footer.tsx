64fd160154e7d7b58a3fd9a5246f4dd10d82e1a6
99uminji <rnalswl96@naver.com>
2024-11-28 15:33:05 +0900

    Design : footer ui 작업

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import s from "./Footer.module.scss";
+
+interface IconGroupProps {
+  icons: { src: string; alt: string }[];
+}
+
+// 아이콘 그룹 컴포넌트
+const IconGroup = ({ icons }: IconGroupProps) => {
+  return (
+    <ul className={s["ft-sns"]}>
+      {icons.map((icon, index) => (
+        <li key={index}>
+          <Link href={icon.src}>
+            <Image src={icon.src} alt={icon.alt} width="20" height="20" style={{ width: "20px", height: "20px" }} />
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 const Footer = () => {
+  const icons = [
+    { src: "/images/icons/icon_email.svg", alt: "이메일" },
+    { src: "/images/icons/icon_facebook.svg", alt: "페이스북" },
+    { src: "/images/icons/icon_instagram.svg", alt: "인스타그램" },
+    { src: "/images/icons/icon_instagram.svg", alt: "인스타그램" },
+  ];
+
   return (
-    <footer>
-      <div className="container"></div>
+    <footer className={s.footer}>
+      <div className="container">
+        <div className={s["ft-inner"]}>
+          <span>©codeit - 2023</span>
+          <ul className={s["ft-info"]}>
+            <li className="mr-8">Privacy Policy</li>
+            <li>FAQ</li>
+          </ul>
+          <IconGroup icons={icons} />
+        </div>
+      </div>
     </footer>
   );
 };
