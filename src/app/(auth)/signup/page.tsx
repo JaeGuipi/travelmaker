dafd0122e6304723cfe8cbbd8451558ea376c1e5
JaeGuipi <jeaseo1997@gmail.com>
2024-11-29 14:15:07 +0900

    develop 브랜치 최신화

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import SignUpForm from "@/components/Auth/SignUpForm";
+
+const SignUp = () => {
+  return (
+    <>
+      <SignUpForm />
+    </>
+  );
+};
+
+export default SignUp;
