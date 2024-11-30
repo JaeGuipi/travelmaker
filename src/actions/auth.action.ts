3e24979bdec068490c794a0ec19de56f68ba0a68
JIHYEMIN <hyemeeeny@gmail.com>
2024-12-01 02:48:24 +0900

    Feat: 서버 액션 폴더 생성 후 추가

---
+++ src/actions/auth.action.ts
@@ -0,0 +1,157 @@
+"use server";
+
+import { revalidateTag } from "next/cache";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+import { PostAuth } from "@/types/auth/authTypes";
+import { PatchMe } from "@/types/users/usersTypes";
+import API_URL from "@/constants/config";
+
+const cookieStore = cookies();
+const accessToken = cookieStore.get("accessToken")?.value;
+
+// 쿠키 저장 함수
+const setCookie = (name: string, value: string) => {
+  const cookieStore = cookies();
+  cookieStore.set({
+    name,
+    value,
+    maxAge: 60 * 60 * 24 * 7, // 일주일 동안 유지
+    path: "/",
+    httpOnly: true,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
+  });
+};
+
+export async function login(loginData: PostAuth) {
+  try {
+    const response = await fetch(`${API_URL}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(loginData),
+    });
+
+    if (!response.ok) {
+      throw new Error("로그인 실패");
+    }
+
+    const data = await response.json();
+
+    setCookie("accessToken", data.accessToken);
+    setCookie("refreshToken", data.refreshToken);
+  } catch (error) {
+    console.log("로그인 중 오류 발생: ", error);
+    throw new Error("로그인 실패");
+  }
+
+  revalidateTag("users");
+  redirect("/");
+}
+
+// 로그아웃
+export const logout = async () => {
+  try {
+    cookieStore.delete("accessToken");
+    cookieStore.delete("refreshToken");
+  } catch (error) {
+    console.error("로그아웃 중 오류 발생: ", error);
+    throw new Error("로그아웃 실패");
+  }
+
+  revalidateTag("user");
+};
+
+// 내 정보 수정
+export const updateUsers = async (data: { profileImageUrl?: string; nickname?: string; newPassword?: string }) => {
+  try {
+    const response = await fetch(`${API_URL}/users/me`, {
+      method: "PATCH",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("내 정보 수정 실패");
+    }
+  } catch (error) {
+    console.error("내 정보 수정 중 오류 발생: ", error);
+    throw new Error("내 정보 수정 실패");
+  }
+
+  revalidateTag("user"); // revalidateTag 넣어줘야지 서버액션에서 버튼 클릭시 바로 반영
+};
+
+// 프로필 이미지 URL 생성
+export const uploadProfileImage = async (formData: FormData) => {
+  const response = await fetch(`${API_URL}/users/me/image`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: formData,
+  });
+
+  if (!response.ok) {
+    throw new Error("이미지 업로드 실패");
+  }
+
+  const data = await response.json();
+  console.log("Upload successful:", data);
+
+  return data.profileImageUrl; // 업로드된 이미지 URL 반환
+};
+
+// export const updateUserProfile = async (profileImageUrl: string) => {
+//   const response = await fetch(`${API_URL}/users/me`, {
+//     method: "PATCH",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${accessToken}`,
+//     },
+//     body: JSON.stringify({ profileImageUrl }),
+//   });
+
+//   if (!response.ok) {
+//     throw new Error("사용자 정보 업데이트 실패");
+//   }
+
+//   return response.json();
+// };
+
+export const authTokens = async () => {
+  const refreshToken = cookies().get("refreshToken")?.value;
+
+  if (!refreshToken) {
+    console.error("refreshToken이 없습니다.");
+    throw new Error("인증되지 않은 사용자");
+  }
+
+  try {
+    const response = await fetch(`${API_URL}/auth/tokens`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${refreshToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      console.error("refreshToken 재발급 에러 발생");
+      throw new Error("refreshToken 재발급 실패");
+    }
+
+    const { accessToken } = await response.json();
+    setCookie("accessToken", accessToken);
+
+    return { success: true, message: "refreshToken 요청 성공" };
+  } catch (error) {
+    console.error("refreshToken 에러 발생: ", error);
+    throw new Error("refreshToken 요청 실패");
+  }
+};
