582b878ce5c8e3eb9053d13b7f24d5044f2f1603
JIHYEMIN <hyemeeeny@gmail.com>
2024-12-01 02:50:03 +0900

    Chore: 토스트 메시지 및 스키마 추가

--- src/schema/zodSchema.ts
+++ src/schema/zodSchema.ts
@@ -3,20 +3,17 @@ import { z } from "zod";
 // 비밀번호 조건 정규표현식
 const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$&*?!%])[A-Za-z\d!@$%&*?]{8,15}$/;
 
-// 로그인 스키마 정의
+export type LoginFormValues = z.infer<typeof LoginSchema>;
+export type signUpSchema = z.infer<typeof signUpSchema>;
+export type MyPageFormValues = z.infer<typeof MyPageSchema>;
+
+// 로그인
 export const LoginSchema = z.object({
   email: z.string().min(1, { message: "이메일을 입력해주세요." }).email({ message: "유효한 이메일을 입력해주세요." }),
-  password: z
-    .string()
-    .min(8, { message: "비밀번호를 8자리 이상 입력해 주세요." })
-    .max(15, { message: "비밀번호를 15자리 이하로 입력해 주세요." })
-    .regex(passwordRegex, {
-      message: "영문, 숫자, 특수문자(~!@#$%^&*)를 모두 조합해 주세요.",
-    }),
+  password: z.string().min(8, "8자 이상 입력해주세요"),
 });
 
-export type LoginFormValues = z.infer<typeof LoginSchema>;
-
+// 회원가입
 export const signUpSchema = z
   .object({
     email: z.string().email("잘못된 이메일입니다"),
@@ -29,4 +26,21 @@ export const signUpSchema = z
     message: "비밀번호가 일치하지 않습니다.",
   });
 
-export type signUpSchema = z.infer<typeof signUpSchema>;
+// 마이페이지
+export const MyPageSchema = z
+  .object({
+    nickname: z.string().min(1, { message: "닉네임을 입력해주세요." }),
+    profileImageUrl: z.string(),
+    newPassword: z
+      .string()
+      .min(8, { message: "비밀번호를 8자리 이상 입력해 주세요." })
+      .max(15, { message: "비밀번호를 15자리 이하로 입력해 주세요." })
+      .regex(passwordRegex, {
+        message: "영문, 숫자, 특수문자(~!@#$%^&*)를 모두 조합해 주세요.",
+      }),
+    newPasswordConfirm: z.string().min(8, "8자 이상 입력해주세요"),
+  })
+  .refine((data) => data.newPassword === data.newPasswordConfirm, {
+    path: ["newPasswordConfirm"],
+    message: "비밀번호가 일치하지 않습니다.",
+  });
