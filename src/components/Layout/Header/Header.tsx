7d75ae22eaa3747226f6fd394bd09dc483f99f03
JIHYEMIN <hyemeeeny@gmail.com>
2024-12-02 20:15:36 +0900

    Refactor: 헤더 수정

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,18 +1,49 @@
+import { cookies } from "next/headers";
+import API_URL from "@/constants/config";
 import Image from "next/image";
 import Link from "next/link";
-import classNames from "classnames/bind";
 import s from "./Header.module.scss";
+import classNames from "classnames/bind";
 import CustomInput from "@/components/Input/CustomInput";
-import { getMyInfo } from "@/lib/api/user";
 import User from "@/components/Layout/Header/User";
 
 export const cx = classNames.bind(s);
 
-const Header = async () => {
-  let user = null;
+const getUsers = async () => {
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+
+  if (!accessToken) {
+    return null;
+  }
 
   try {
-    user = await getMyInfo();
+    const response = await fetch(`${API_URL}/users/me`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      next: { tags: ["users"] },
+      cache: "no-store",
+    });
+
+    if (!response.ok) {
+      return null;
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("유저 정보 가져오기 실패:", error);
+    return null;
+  }
+};
+
+const Header = async () => {
+  let users = null;
+
+  try {
+    users = await getUsers();
   } catch (error) {
     console.error("유저 정보 요청 실패", error);
   }
@@ -32,7 +63,15 @@ const Header = async () => {
           placeholder="무엇을 체험하고 싶으신가요?"
         />
       </Link>
-      <User user={user} />
+
+      {!users ? (
+        <div className={s.loginOff}>
+          <Link href={"/login"}>로그인</Link>
+          <Link href={"/signup"}>회원가입</Link>
+        </div>
+      ) : (
+        <User users={users} />
+      )}
     </header>
   );
 };
