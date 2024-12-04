5471a3f4e341d1d6c8e96541ec9e939f787934e3
jjeankim <miyukikj35@gmail.com>
2024-12-04 20:54:24 +0900

    Fix:pr 리뷰 반영

--- src/app/(usercard)/my-reservation/page.tsx
+++ src/app/(usercard)/my-reservation/page.tsx
@@ -1,23 +1,23 @@
-// import API_URL from "@/constants/config";
+import API_URL from "@/constants/config";
 import s from "./page.module.scss";
 import classNames from "classnames/bind";
-// import { MyReservation } from "@/types/types";
-// import MyReservationList from "@/components/MyReservationList/MyReservationList";
-// import { customFetch } from "@/utils/customFetch";
+import { MyReservation } from "@/types/types";
+import MyReservationList from "@/components/MyReservationList/MyReservationList";
+import { customFetch } from "@/utils/customFetch";
 
 const cx = classNames.bind(s);
 
 const MyReservationPage = async () => {
 
-  // const response = await customFetch(`${API_URL}/my-reservations?size=6`, {
-  // });
+  const response = await customFetch(`${API_URL}/my-reservations?size=6`, {
+  });
 
-  // if (!response.ok) {
-  //   console.error("예약 데이터를 불러오는 데 실패했습니다.");
-  // }
-  // const myReservation = await response.json();
-  // const cursorId = myReservation.cursorId;
-  // const reservationList: MyReservation[] = myReservation.reservations;
+  if (!response.ok) {
+    console.error("예약 데이터를 불러오는 데 실패했습니다.");
+  }
+  const myReservation = await response.json();
+  const cursorId = myReservation.cursorId;
+  const reservationList: MyReservation[] = myReservation.reservations;
 
   return (
     // <section className={cx("container", "reservation")}>
@@ -25,7 +25,7 @@ const MyReservationPage = async () => {
         <div className={cx("list-title-container")}>
           <h2 className={cx("list-title")}>예약 내역</h2>
         </div>
-        {/* <MyReservationList initialReservations={reservationList} cursorId={cursorId} /> */}
+        <MyReservationList initialReservations={reservationList} cursorId={cursorId} />
       </div>
     // </section>
   );
--- src/components/ItemLayout/ItemLayout.tsx
+++ src/components/ItemLayout/ItemLayout.tsx
@@ -1,14 +1,11 @@
 import s from "./ItemLayout.module.scss";
-import classNames from "classnames/bind";
 import Image from "next/image";
 import { ReactNode } from "react";
 
-const cx = classNames.bind(s);
-
 const ItemLayout = ({ children, src, alt }: { children: ReactNode; src: string; alt: string }) => {
   return (
-    <div className={cx("item-container")}>
-      <div className={cx("img-container")}>
+    <div className={s["item-container"]}>
+      <div className={s["img-container"]}>
         <Image src={src} width={204} height={204} alt={alt} className={s.img} />
       </div>
       {children}
