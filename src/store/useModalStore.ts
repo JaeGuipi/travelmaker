7adb224dca5f38d018ec259510d4946dc4592270
99uminji <rnalswl96@naver.com>
2024-12-05 21:51:26 +0900

    Refactor: 모달 수정

--- src/store/useModalStore.ts
+++ src/store/useModalStore.ts
@@ -1,16 +1,18 @@
 import { create } from "zustand";
 
 interface ModalStore {
-  modals: { [key: string]: boolean }; // 고유한 텍스트(또는 타이틀)로 관리
-  toggleModal: (modalKey: string) => void; // 텍스트 또는 타이틀로 모달을 토글
+  modals: { [key: string]: boolean };
+  toggleModal: (modalKey: string) => void;
 }
 
 const useModalStore = create<ModalStore>((set) => ({
   modals: {},
-  toggleModal: (modalKey) =>
-    set((state) => ({
-      modals: { ...state.modals, [modalKey]: !state.modals[modalKey] }, // 텍스트로 상태 관리
-    })),
+  toggleModal: (modalKey: string) =>
+    set((state) => {
+      const newModals = { ...state.modals };
+      newModals[modalKey] = !newModals[modalKey];
+      return { modals: newModals };
+    }),
 }));
 
 export default useModalStore;
