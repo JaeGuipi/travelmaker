24c425438303ba94d5e29f8cb34d21a497e93aaf
JIHYEMIN <hyemeeeny@gmail.com>
2024-12-06 15:31:11 +0900

    Refactor: input TextArea 추가

--- src/lib/toastMessage.ts
+++ src/lib/toastMessage.ts
@@ -4,12 +4,14 @@ const toastMessages = {
     signup: "회원가입에 성공하였습니다.",
     editInfo: "내 정보가 수정되었습니다.",
     reservation: "예약이 완료되었습니다.",
+    activity: "체험이 등록되었습니다.",
   },
   error: {
     login: "로그인에 실패하였습니다.",
     signup: "회원가입에 실패하였습니다.",
     editInfo: "내 정보 수정에 실패하였습니다.",
     requestLogin: "로그인 후 이용해주세요.",
+    activity: "체험 등록에 실패하였습니다.",
   },
 };
 
