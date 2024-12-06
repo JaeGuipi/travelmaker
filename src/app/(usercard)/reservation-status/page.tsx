d8433c204c4439a4e0d41e00628ce2a6f757123d
JaeGuipi <81738650+JaeGuipi@users.noreply.github.com>
2024-12-06 14:03:04 +0900

    미들웨어 개발중인 페이지 추가

--- src/app/(usercard)/reservation-status/page.tsx
+++ src/app/(usercard)/reservation-status/page.tsx
@@ -1,17 +1,21 @@
-"use client";
+import API_URL from "@/constants/config";
+import { customFetch } from "@/utils/customFetch";
 import { cookies } from "next/headers";
-import { useRouter } from "next/navigation";
 
-const Page = () => {
-  const router = useRouter();
+const Page = async () => {
   const cookieStore = cookies();
   const accessToken = cookieStore.get("accessToken");
 
   if (!accessToken) {
-    return router.push("/login");
+    return <div>로그인이 필요한 페이지 입니다.</div>;
   }
 
-  return <div>예약 내역 페이지 입니다....</div>;
+  const response = await customFetch(`${API_URL}/my-activities`, {
+    cache: "no-cache",
+  });
+
+  const data = await response.json();
+  return <div></div>;
 };
 
 export default Page;
