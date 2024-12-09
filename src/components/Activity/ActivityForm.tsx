72d2f145650b73b5c82df6ad07e901cdf1becfcf
JIHYEMIN <hyemeeeny@gmail.com>
2024-12-09 21:57:30 +0900

    Refactor: 체험 수정, 등록 공용 폼으로 수정

--- src/components/Activity/ActivityForm.tsx
+++ src/components/Activity/ActivityForm.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { ChangeEvent, useState } from "react";
-import { useRouter } from "next/navigation";
 import s from "./ActivityForm.module.scss";
+import { ChangeEvent, useEffect, useState } from "react";
+import { useRouter, useParams } from "next/navigation";
 import { Controller, useForm } from "react-hook-form";
-import { postActivity, uploadActivityImage } from "@/actions/activity.action";
-import { PostActivity } from "@/types/activites/activitesTypes";
+import { getActivityById, postActivity, updateActivity, uploadActivityImage } from "@/actions/activity.action";
+import { PostActivity, SubImage } from "@/types/activites/activitesTypes";
 import { useToast } from "@/hooks/useToast";
 import toastMessages from "@/lib/toastMessage";
 import FormButton from "@/components/Button/FormButton";
@@ -13,19 +13,23 @@ import CustomInput from "@/components/Input/CustomInput";
 import ScheduleInput from "@/components/Input/ScheduleInput";
 import BannerInput from "@/components/Input/BannerInput";
 import CategoryDropdown from "@/components/Dropdown/CategoryDropdown";
+import LoadingSpinner from "@/components/LoadingSpinner/LoadingSpinner";
 
 const ActivityForm = () => {
   const { showSuccess, showError, notify } = useToast();
   const router = useRouter();
+  const { activityId } = useParams();
   const [selectedBannerFile, setSelectedBannerFile] = useState<File | null>(null);
   const [selectedSubFiles, setSelectedSubFiles] = useState<File[]>([]);
   const [bannerPreview, setBannerPreview] = useState<string>("");
   const [subImagePreviews, setSubImagePreviews] = useState<string[]>([]);
+  const [activity, setActivity] = useState<PostActivity | null>(null);
   const {
     register,
     handleSubmit,
     control,
-    formState: { errors },
+    formState: { errors, isValid },
+    reset,
   } = useForm<PostActivity>({
     defaultValues: {
       title: "",
@@ -33,41 +37,56 @@ const ActivityForm = () => {
       description: "",
       address: "",
       price: 0,
-      schedules: [{ date: "", startTime: "", endTime: "" }],
+      schedules: [],
       bannerImageUrl: "",
       subImageUrls: [""],
     },
   });
 
+  // 데이터 가져오기 및 폼 초기화
+  useEffect(() => {
+    const fetchActivities = async () => {
+      if (activityId) {
+        const id = Number(activityId);
+        const selectedActivity = await getActivityById(id);
+        if (selectedActivity) {
+          setActivity(selectedActivity);
+          reset({ ...selectedActivity });
+          setBannerPreview(selectedActivity.bannerImageUrl || "");
+          setSubImagePreviews(selectedActivity.subImages?.map((img: SubImage) => img.imageUrl) || []);
+        }
+      }
+    };
+    fetchActivities();
+  }, [activityId, reset]);
+
+  // 배너 이미지 체인지 이벤트
   const handleBannerImageChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files || e.target.files.length === 0) return;
-
     const file = e.target.files[0];
     setSelectedBannerFile(file);
     setBannerPreview(URL.createObjectURL(file));
   };
 
+  // 소개 이미지 체인지 이벤트
   const handleSubImageChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
-
     const files = Array.from(e.target.files);
     if (selectedSubFiles.length + files.length > 4) {
       notify(toastMessages.notify.imageUpload);
       return;
     }
-
-    const newFiles = [...selectedSubFiles, ...files];
-    const newPreviews = [...subImagePreviews, ...files.map((file) => URL.createObjectURL(file))];
-
-    setSelectedSubFiles(newFiles);
-    setSubImagePreviews(newPreviews);
+    setSelectedSubFiles([...selectedSubFiles, ...files]);
+    setSubImagePreviews([...subImagePreviews, ...files.map((file) => URL.createObjectURL(file))]);
   };
 
+  // 배너 이미지 삭제 이벤트
   const handleBannerImageRemove = () => {
     setSelectedBannerFile(null);
     setBannerPreview("");
   };
 
+  // 소개 이미지 삭제 이벤트
   const handleSubImageRemove = (index: number) => {
     setSelectedSubFiles((prev) => prev.filter((_, i) => i !== index));
     setSubImagePreviews((prev) => prev.filter((_, i) => i !== index));
@@ -77,13 +96,19 @@ const ActivityForm = () => {
     try {
       let activityImageUrl = bannerPreview;
       const subImageUrls: string[] = [];
+      const subImageIdsToRemove: number[] = [];
+      const subImageUrlsToAdd: string[] = [];
+      const scheduleIdsToRemove: number[] = [];
+      const schedulesToAdd: Array<{ date: string; startTime: string; endTime: string }> = [];
 
+      // 배너 이미지 업로드
       if (selectedBannerFile) {
         const formData = new FormData();
         formData.append("image", selectedBannerFile);
         activityImageUrl = await uploadActivityImage(formData);
       }
 
+      // 소개 이미지 업로드
       if (selectedSubFiles.length > 0) {
         for (const file of selectedSubFiles) {
           const formData = new FormData();
@@ -93,35 +118,78 @@ const ActivityForm = () => {
         }
       }
 
+      if (activityId && activity) {
+        const activityData = activity;
+        const currentScheduleIds = activityData.schedules.map((schedule) => schedule.id);
+
+        // 새로운 스케줄 추가
+        data.schedules.forEach((schedule) => {
+          if (!currentScheduleIds.includes(schedule.id)) {
+            schedulesToAdd.push(schedule);
+          }
+        });
+
+        // 기존 스케줄 id 삭제
+        scheduleIdsToRemove.push(
+          ...activityData.schedules
+            .filter((schedule) => !data.schedules.some((newSchedule) => newSchedule.id === schedule.id))
+            .map((schedule) => schedule.id),
+        );
+
+        // 새로운 소개 이미지 추가
+        subImageUrlsToAdd.push(...subImageUrls);
+
+        // 기존 소개 이미지 id 삭제
+        if (activityData.subImages) {
+          subImageIdsToRemove.push(
+            ...activityData.subImages
+              .filter((img) => img.imageUrl && !subImagePreviews.includes(img.imageUrl))
+              .map((img) => img.id),
+          );
+        }
+      }
+
       const updatedData = {
         ...data,
         bannerImageUrl: activityImageUrl,
         subImageUrls: subImageUrls,
+        subImageIdsToRemove,
+        subImageUrlsToAdd,
+        scheduleIdsToRemove,
+        schedulesToAdd,
       };
 
-      console.log("폼 데이터:", updatedData);
+      if (activityId) {
+        // 체험 수정
+        const id = Number(activityId);
+        await updateActivity(id, updatedData);
+        showSuccess(toastMessages.success.activityUpdate);
+      } else {
+        // 체험 등록
+        await postActivity(updatedData);
+        showSuccess(toastMessages.success.activity);
+      }
 
-      console.log("POST 요청 보내기 준비됨:", updatedData);
-      await postActivity(updatedData);
-      showSuccess(toastMessages.success.activity);
       router.push("/my-activities");
-      console.log("POST 요청 전송 완료");
     } catch (error) {
       console.error(error);
       showError(toastMessages.error.activity);
     }
   };
 
+  if (activityId && !activity) {
+    return <LoadingSpinner />;
+  }
+
   return (
     <section className={s.activityForm}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className={s.activityFormHeader}>
-          <h2>내 체험 등록</h2>
-          <FormButton type="submit" size="medium">
-            등록하기
+          <h2>{activityId ? "내 체험 수정" : "내 체험 등록"}</h2>
+          <FormButton type="submit" size="medium" disabled={!isValid}>
+            {activityId ? "수정하기" : "등록하기"}
           </FormButton>
         </div>
-
         <div className={s.activityFormBody}>
           <CustomInput id="title" type="text" placeholder="제목" {...register("title")} />
           <Controller
