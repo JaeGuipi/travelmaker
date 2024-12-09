5ab1accd427088ca2fa7aacb3482172edae41da0
99uminji <rnalswl96@naver.com>
2024-12-09 12:00:23 +0900

    Fix: 메인 의존성 추가

--- src/components/Home/ActivityListSection/ActivityList.tsx
+++ src/components/Home/ActivityListSection/ActivityList.tsx
@@ -4,12 +4,12 @@ import { GetActivities } from "@/types/activites/activitesTypes";
 import { getActivity } from "@/lib/api/activities";
 import { Activity } from "@/types/types";
 import { useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
 import s from "./ActivityList.module.scss";
 import classNames from "classnames/bind";
 import CategoryList from "./CategoryList";
 import AllItemList from "./AllItemList";
 import LoadingSpinner from "@/components/LoadingSpinner/LoadingSpinner";
-import { useMediaQuery } from "react-responsive";
 
 export const cx = classNames.bind(s);
 
@@ -24,15 +24,17 @@ const ActivityList: React.FC<GetActivities> = ({ activities }) => {
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1200 });
   const isMobile = useMediaQuery({ maxWidth: 767 });
 
-  // 화면 크기에 따라 초기 size 설정
+  // 화면 크기에 따라 초기 size 반환
+  const getInitialSize = (isPC: boolean, isTablet: boolean, isMobile: boolean) => {
+    if (isPC) return 8;
+    if (isTablet) return 9;
+    if (isMobile) return 4;
+    return 8; // 기본값
+  };
+
+  // 초기 size 설정
   useEffect(() => {
-    if (isPC) {
-      setSize(8);
-    } else if (isTablet) {
-      setSize(9);
-    } else if (isMobile) {
-      setSize(4);
-    }
+    setSize(getInitialSize(isPC, isTablet, isMobile));
   }, [isPC, isTablet, isMobile]);
 
   // 카테고리 목록 조회
@@ -41,30 +43,31 @@ const ActivityList: React.FC<GetActivities> = ({ activities }) => {
       ? ["전체", ...new Set(activities.map((activity) => activity.category))]
       : ["전체"];
 
+  const fetchFilteredActivities = async (newSize: number) => {
+    try {
+      // "전체" 카테고리는 params에 포함시키지 않고 요청
+      const params = selectedCategory !== "전체" ? { category: selectedCategory, size: newSize } : { size: newSize };
+      const data = await getActivity(params);
+      setFilteredActivities(data.activities); // 필터링된 데이터 업데이트
+    } catch (error) {
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
+
   // 카테고리에 해당되는 리스트 조회
   useEffect(() => {
-    const fetchFilteredActivities = async (newSize: number) => {
-      try {
-        // "전체" 카테고리는 params에 포함시키지 않고 요청
-        const params = selectedCategory !== "전체" ? { category: selectedCategory, size: newSize } : { size: newSize };
-        const data = await getActivity(params);
-        setFilteredActivities(data.activities); // 필터링된 데이터 업데이트
-      } catch (error) {
-        throw error;
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    // 카테고리가 변경되었을 때만 데이터를 요청
-    if (selectedCategory) {
+    if (size > 0) {
       fetchFilteredActivities(size);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedCategory, size]);
 
   // 카테고리 클릭 핸들러
   const handleCategoryClick = (category: string) => {
     setSelectedCategory(category);
+    setSize(getInitialSize(isPC, isTablet, isMobile)); // 카테고리 변경 시 size를 초기화
   };
 
   // "더보기" 클릭 시 호출
