efcc54eeddd14f4fe3869aea720b5bc82b58fedd
JIHYEMIN <hyemeeeny@gmail.com>
2024-12-18 00:09:51 +0900

    Chore: 아이콘 및 텍스트 수정정

--- src/components/UserTab/UserTabList.tsx
+++ src/components/UserTab/UserTabList.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import s from "./UserTabList.module.scss";
-import Link from "next/link";
-
-import { RiUserSettingsLine } from "react-icons/ri";
-import { GoChecklist } from "react-icons/go";
-import { AiOutlineSetting } from "react-icons/ai";
-import { TbCalendarCheck } from "react-icons/tb";
 import { usePathname } from "next/navigation";
+import Link from "next/link";
+import s from "./UserTabList.module.scss";
+import {
+  HiOutlineUser,
+  HiOutlineClipboardDocumentCheck,
+  HiOutlineCog6Tooth,
+  HiOutlineCalendarDays,
+} from "react-icons/hi2";
 
 const UserTabList = () => {
   const pathname = usePathname();
@@ -15,20 +16,20 @@ const UserTabList = () => {
   return (
     <div className={s.userTabList}>
       <Link href="/my-info" className={pathname.startsWith("/my-info") ? s.active : ""}>
-        <RiUserSettingsLine />내 프로필
+        <HiOutlineUser />내 프로필
       </Link>
 
       <Link href="/my-reservation" className={pathname.startsWith("/my-reservation") ? s.active : ""}>
-        <GoChecklist />
+        <HiOutlineClipboardDocumentCheck />
         예약 내역
       </Link>
 
       <Link href="/my-activities" className={pathname.startsWith("/my-activities") ? s.active : ""}>
-        <AiOutlineSetting />내 체험 관리
+        <HiOutlineCog6Tooth />내 체험 관리
       </Link>
 
       <Link href="/reservation-status" className={pathname.startsWith("/reservation-status") ? s.active : ""}>
-        <TbCalendarCheck />
+        <HiOutlineCalendarDays />
         예약 현황
       </Link>
     </div>
