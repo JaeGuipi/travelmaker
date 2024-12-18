bbfe9e01696c89700a4c191c632bed3430c60cc2
jjeankim <miyukikj35@gmail.com>
2024-12-19 08:48:31 +0900

    Fix: cookie 로직 중복되어 customfetch fetch로 변경

--- src/components/BottomBar/BottomBar.tsx
+++ src/components/BottomBar/BottomBar.tsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 import { IoSearch, IoPersonOutline } from "react-icons/io5";
 import { TbReservedLine } from "react-icons/tb";
 import { AiOutlineHome } from "react-icons/ai";
-import { customFetch } from "@/utils/customFetch";
 import API_URL from "@/constants/config";
 import Alarm from "../Alarm/Alarm";
 import { cookies } from "next/headers";
@@ -16,7 +15,12 @@ const BottomBar = async () => {
 
   if (!accessToken) return;
 
-  const response = await customFetch(`${API_URL}/my-notifications?size=4`);
+  const response = await fetch(`${API_URL}/my-notifications?size=4`,{
+    headers: {
+      Authorization: `Barer ${accessToken}`
+    }
+  });
+
   if (!response.ok) {
     console.error("하단바 내 알림 리스트 조회 실패");
   }
