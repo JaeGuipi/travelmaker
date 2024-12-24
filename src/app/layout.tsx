7dcb1ff89f32cbf8c9daf522159339b69bed3b8d
JIHYEMIN <hyemeeeny@gmail.com>
2024-12-24 14:52:56 +0900

    Chore: 레이아웃 타이틀 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,10 @@ import BottomBar from "@/components/BottomBar/BottomBar";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "travel maker",
+  title: "TRAVEL MAKER",
   description: "트레블 메이커에서 즐기는 다양한 체험",
   openGraph: {
-    title: "travel maker",
+    title: "TRAVEL MAKER",
     description: "트레블 메이커에서 즐기는 다양한 체험",
     images: [
       {
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
       },
     ],
     url: "https://travelmaker-ten.vercel.app",
-    siteName: "travel maker",
+    siteName: "TRAVEL MAKER",
     locale: "ko_KR",
     type: "website",
   },
