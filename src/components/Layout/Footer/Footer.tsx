a89641f718c315e4f2ed5c61b783f6cb9006f10d
99uminji <rnalswl96@naver.com>
2024-12-26 17:53:53 +0900

    Fix: 푸터 svg 오류 해결

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import s from "./Footer.module.scss";
 
 interface IconGroupProps {
-  icons: { src: string; alt: string }[];
+  icons: { src: string; alt: string; link: string }[];
 }
 
 // 아이콘 그룹 컴포넌트
@@ -12,7 +12,7 @@ const IconGroup = ({ icons }: IconGroupProps) => {
     <ul className={s["ft-sns"]}>
       {icons.map((icon, index) => (
         <li key={index}>
-          <Link href={icon.src}>
+          <Link href={icon.link} target="_blank" rel="noopener noreferrer">
             <Image src={icon.src} alt={icon.alt} width="20" height="20" style={{ width: "20px", height: "20px" }} />
           </Link>
         </li>
@@ -23,10 +23,10 @@ const IconGroup = ({ icons }: IconGroupProps) => {
 
 const Footer = () => {
   const icons = [
-    { src: "/icons/ft_facebook.svg", alt: "페이스북" },
-    { src: "/icons/ft_twitter.svg", alt: "트위터" },
-    { src: "/icons/ft_youtube.svg", alt: "유투브" },
-    { src: "/icons/ft_instagram.svg", alt: "인스타그램" },
+    { src: "/icons/ft_facebook.svg", alt: "페이스북", link: "https://facebook.com" },
+    { src: "/icons/ft_twitter.svg", alt: "트위터", link: "https://twitter.com" },
+    { src: "/icons/ft_youtube.svg", alt: "유투브", link: "https://youtube.com" },
+    { src: "/icons/ft_instagram.svg", alt: "인스타그램", link: "https://instagram.com" },
   ];
 
   return (
