b3cbda346a0cf656dfb231d3c65e5248c0e26bc2
JaeGuipi <jeaseo1997@gmail.com>
2025-03-28 17:39:24 +0900

    Feat: LCP향상을위해 카테고리 이미지 파일 확장자 수정

--- src/components/Home/VisualSection/MainSwiper.tsx
+++ src/components/Home/VisualSection/MainSwiper.tsx
@@ -29,7 +29,13 @@ export default function MainSwiper() {
             <SwiperText slideIndex={0} />
             <picture>
               <source media="(max-width: 1200px)" srcSet="/images/tb_main_bnr01.jpg" />
-              <Image src="/images/main_bnr01.jpg" fill alt="main_bnr01" priority />
+              <Image
+                src="/images/main_bnr01.jpg"
+                sizes="(max-width: 1200px) 100vw, 1200px"
+                fill
+                alt="main_bnr01"
+                priority
+              />
             </picture>
           </SwiperSlide>
           <SwiperSlide>
@@ -37,14 +43,26 @@ export default function MainSwiper() {
 
             <picture>
               <source media="(max-width: 1200px)" srcSet="/images/tb_main_bnr02.jpg" />
-              <Image src="/images/main_bnr02.jpg" fill alt="main_bnr02" loading="eager" />
+              <Image
+                src="/images/main_bnr02.jpg"
+                sizes="(max-width: 1200px) 100vw, 1200px"
+                fill
+                alt="main_bnr02"
+                loading="eager"
+              />
             </picture>
           </SwiperSlide>
           <SwiperSlide>
             <SwiperText slideIndex={2} />
             <picture>
               <source media="(max-width: 1200px)" srcSet="/images/tb_main_bnr03.jpg" />
-              <Image src="/images/main_bnr03.jpg" fill alt="main_bnr03" loading="eager" />
+              <Image
+                src="/images/main_bnr03.jpg"
+                sizes="(max-width: 1200px) 100vw, 1200px"
+                fill
+                alt="main_bnr03"
+                loading="eager"
+              />
             </picture>
           </SwiperSlide>
           <div className={s["swiper-prev"]}>
